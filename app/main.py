189e2b40d6162812de66e881a9c951f10253df28
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-05 22:07:44 -0400

    added global exeption handler

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.api.v1.roomsController import RoomsController
 from app.repositories.room_repository import RoomRepository
 from app.core.db_config import db_plugin
 from app.core.security import jwt_auth
+from app.core.exceptions import GLOBAL_EXCEPTION_HANDLERS
 
 async def provide_room_repo(db_session: AsyncSession) -> RoomRepository:
     return RoomRepository(session=db_session)
@@ -18,5 +19,6 @@ app = Litestar(
     dependencies={
         "room_repo": Provide(provide_room_repo)
     },
+    exception_handlers=GLOBAL_EXCEPTION_HANDLERS,
     debug=False
 )
