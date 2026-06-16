17e046f4b4f72a11d1140d78cc3510e7b65bacd1
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2026-06-15 23:24:08 -0400

    hotfix

--- app/core/db_config.py
+++ app/core/db_config.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 from advanced_alchemy.extensions.litestar import SQLAlchemyPlugin, SQLAlchemyAsyncConfig
+from app.models.base import Base
+from app.models.room import RoomModel
+from app.models.room_member import RoomMemberModel
 
 load_dotenv(override=True)
 
@@ -10,7 +13,8 @@ DATABASE_URL = os.getenv(
 
 db_config = SQLAlchemyAsyncConfig(
     connection_string= DATABASE_URL,
-    create_all=True
+    create_all=True,
+    metadata=Base.metadata
 )
 
 db_plugin = SQLAlchemyPlugin(config=db_config)
